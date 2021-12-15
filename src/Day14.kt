7434a39c1cfb440ae72624dcfa0d5929b8b4fe73
Marek Kohn <me@makohn.de>
2021-12-15 02:28:48 +0100

    Add solution for day 14 part 2

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,23 +1,24 @@
 fun main() {
 
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>): Long {
         val (template, rules) = Day14Helper.parse(input)
-        val polymer = Day14Helper.buildPolymer(template, rules)
-        val occurrences = polymer.groupingBy { it }.eachCount().values
-        return occurrences.maxOf { it } - occurrences.minOf { it }
+        val occurrences = Day14Helper.simulatePolymer(template, rules, steps = 10).values
+        return 0L + occurrences.maxOf { it } - occurrences.minOf { it }
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        val (template, rules) = Day14Helper.parse(input)
+        val occurrences = Day14Helper.simulatePolymer(template, rules).values
+        return 0L + occurrences.maxOf { it } - occurrences.minOf { it }
     }
 
     val testInput = readInput("Day14_test")
-    check(part1(testInput) == 1588)
-//    check(part2(testInput) == 16)
+    check(part1(testInput) == 1588L)
+    check(part2(testInput) == 2188189693529)
 
     val input = readInput("Day14")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
 
 object Day14Helper {
@@ -30,13 +31,30 @@ object Day14Helper {
         return template to rules
     }
 
-    fun buildPolymer(template: String, rules: Map<String, String>, step: Int = 10): String {
+    fun buildPolymer(template: String, rules: Map<String, String>, steps: Int = 10): String {
         var polymer = template
-        (0 until step).forEach { _ ->
+        repeat(steps) {
             polymer = polymer.windowed(2) {
                 if (rules.contains(it)) "${it[0]}${rules[it]}${it[1]}" else it
-            }.foldRight("") { acc, e -> "${acc}${e.drop(1)}"}
+            }.also { println(it) }.foldRight("") { acc, e -> "${acc}${e.drop(1)}"}
         }
         return polymer
+    }
+
+    fun simulatePolymer(template: String, rules: Map<String, String>, steps: Int = 40): Map<String, Long> {
+        val pairRules = rules.map { it.key to (it.key[0]+it.value to it.value+it.key[1]) }.toMap()
+        var polymer = template.windowed(2).groupingBy { it }.fold(0L) { acc, _ -> acc + 1 }
+        repeat(steps) {
+            val newPolymer = mutableMapOf<String, Long>()
+            polymer.entries.forEach { entry ->
+                pairRules[entry.key]?.toList()?.forEach { rule ->
+                   newPolymer[rule] = newPolymer.getOrDefault(rule, 0) + entry.value
+                }
+            }
+            polymer = newPolymer
+        }
+        return polymer.flatMap { listOf(it.key[0].toString() to it.value, it.key[1].toString() to it.value) }
+            .groupingBy { it.first }
+            .fold(0L) { acc, e -> acc + (e.second/2.0).toLong() }
     }
 }
