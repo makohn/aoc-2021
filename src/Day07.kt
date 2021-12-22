be8588fd8f7ce232af5dabdb7b9745b23f122c7f
Marek Kohn <me@makohn.de>
2021-12-22 18:12:25 +0100

    Add solution for day 7 part 2

--- src/Day07.kt
+++ src/Day07.kt
@@ -5,23 +5,38 @@ fun main() {
     val day = "07"
 
     fun part1(input: List<String>): Int {
-        val positions = input.first().split(",").map { it.toInt() }
-        val costs = mutableListOf<Int>()
-        for (goal in (1..positions.maxOf { it })) {
-            costs.add(positions.sumOf { pos -> abs(pos - goal) })
+        val positions = parse(input)
+        return minimizeCost(positions) { goal ->
+            positions.sumOf { pos -> abs(pos - goal) }
         }
-        return costs.minOf { it }
     }
 
     fun part2(input: List<String>): Int {
-        return -1
+        val positions = parse(input)
+        return minimizeCost(positions) { goal ->
+            positions.sumOf { pos ->
+                var sum = 0
+                for (j in (1 .. abs(pos - goal))) sum += j
+                sum
+            }
+        }
     }
 
     val testInput = readInput("Day${day}_test")
     check(part1(testInput) == 37)
-//    check(part2(testInput) == 0)
+    check(part2(testInput) == 168)
 
     val input = readInput("Day${day}")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
+}
+
+private fun parse(input: List<String>) = input.first().split(",").map { it.toInt() }
+
+private fun minimizeCost(positions: List<Int>, costFunction: (Int) -> Int): Int {
+    val costs = mutableListOf<Int>()
+    for (goal in (1..positions.maxOf { it })) {
+        costs.add(costFunction.invoke(goal))
+    }
+    return costs.minOf { it }
 }
