2a5116bf2677b05444d3ef7c7d4024f4202f5819
Marek Kohn <me@makohn.de>
2021-12-24 23:42:26 +0100

    Add (interim) solution for day 23 part 1

--- src/Day23.kt
+++ src/Day23.kt
@@ -1,4 +1,6 @@
 import java.util.*
+import kotlin.collections.HashMap
+import kotlin.collections.HashSet
 import kotlin.math.abs
 
 fun main() {
@@ -6,31 +8,100 @@ fun main() {
     val day = "23"
 
     fun part1(input: List<String>): Int {
-        val hallway = input[1].removeSurrounding("#").map { it }
-        val amphipods = input.subList(2, 4).map { it.toCharArray().filter { c -> c != '#' && c != ' ' }}
-        val rooms = amphipods[0].zip(amphipods[1]).map { listOf(it.first, it.second) }
+        val distances = HashMap<Situation, Int>()
+        val queue = PriorityQueue(compareBy(Situation::distance))
+        val visited = HashSet<Situation>()
 
-        val startSituation = Situation(hallway, rooms)
-        println(startSituation)
-        println(rooms)
-
-        val visited = mutableSetOf<Situation>()
-        val queue = PriorityQueue<Situation>().apply { add(startSituation) }
-
-        var count = 10
-        while (true) {
-            count--
-            val situation = queue.remove()
-            if (situation.final()) {
-                println("Win: ${situation.cost}")
-                break
-            }
-            println("Total dist: ${situation.cost}")
-            if (situation in visited) continue
-            visited.add(situation)
-            transform(situation).forEach { queue.add(it) }
+        fun enqueue(situation: Situation) {
+            val startDistance = distances[situation] ?: Int.MAX_VALUE
+            if (situation.distance >= startDistance) return
+            distances[situation] = situation.distance
+            queue += situation
         }
-        return -1
+
+        val input1 = input.subList(1, 4)
+        val start = Situation(input1.map { it.toCharArray() }.toTypedArray(), 0)
+
+        println(start)
+        enqueue(start)
+
+        fun cost(amphipod: Char) = when(amphipod) {
+            'A' -> 1
+            'B' -> 10
+            'C' -> 100
+            'D' -> 1000
+            else -> error("$amphipod")
+        }
+
+        while (true) {
+            val situation = queue.remove()!!
+            if (situation in visited) continue
+            visited += situation
+            val startDistance = distances[situation]!!
+            if (visited.size % 10000 == 0) println("d=$startDistance qs=${queue.size}, fs=${visited.size}")
+            var ok = true
+
+            // Check for each room line r, if for each room the correct amphipod is already there
+            check@for (r in 1..2) for (i in 0 .. 3) if (situation.config[r][2 * i + 3] != 'A' + i) {
+                ok = false
+                break@check
+            }
+            if (ok) {
+                println(situation)
+                return situation.distance
+            }
+
+            // Check the hallway
+            for (hallwayPos in 1..11) {
+                val hallwayContent = situation.config[0][hallwayPos]
+                if (hallwayContent !in 'A'..'D') continue // If hallway does not contain amphipod at position
+                val amphipodInt = (hallwayContent - 'A')
+                val homeRoom = 2 * amphipodInt + 3
+
+                // If hallway between current pos and homeRoom entrance is not free
+                if (!(minOf(hallwayPos, homeRoom) + 1 until maxOf(hallwayPos, homeRoom)).all {
+                    x -> situation.config[0][x] == '.'  }) continue
+
+                // If the home room for the current amphipod (also hallwayContent) is not empty or occupied by same-named amphipod
+                if (!(1..2).all { r -> situation.config[r][homeRoom] == '.' ||
+                    situation.config[r][homeRoom] == hallwayContent }) continue
+
+                // Find the first free space in the amphipod's home room
+                val freeSpace = (2 downTo 1).first { space -> situation.config[space][homeRoom] == '.' }
+                val newSituation = situation.copy(
+                    // prev situation's cost   amphipod factor         entrance - current hallway   pos in room
+                    startDistance + cost(hallwayContent) * (abs(homeRoom - hallwayPos) + freeSpace)
+                )
+                newSituation.config[0][hallwayPos] = '.'
+                newSituation.config[freeSpace][homeRoom] = hallwayContent
+                enqueue(newSituation)
+            }
+
+            // Go through all rooms (and their positions)
+            for (i in 0..3) for (r in 1..2) {
+                val homeRoom = 2 * i + 3
+                val currentRoomPosContent = situation.config[r][homeRoom]
+                // If current content is not an amphipod
+                if (currentRoomPosContent !in 'A'..'D') continue
+                // If the upper space in the room is not empty
+                if (r == 2 && situation.config[1][homeRoom] != '.') continue
+                for (hallwayPos in 1..11) {
+                    // If the current pos is a room entrance
+                    if ((hallwayPos - 3) % 2 == 0 && (hallwayPos - 3) / 2 in 0..3) continue
+                    // If the path between current pos and homeRoom entry is no empty
+                    if (!(minOf(hallwayPos, homeRoom)..maxOf(hallwayPos, homeRoom)).all {
+                        cell -> situation.config[0][cell] == '.' }) continue
+                    val newSituation = situation.copy(
+                            // prev situation's cost    amphipod's cost          distance between current hallway and home entry     pos in room
+                        startDistance + cost(currentRoomPosContent) * (abs(hallwayPos - homeRoom) + r)
+                    )
+                    // Walk from current room pos to (current) hallway pos
+                    newSituation.config[r][homeRoom] = '.'
+                    newSituation.config[0][hallwayPos] = currentRoomPosContent
+                    enqueue(newSituation)
+                }
+            }
+        }
     }
 
     fun part2(input: List<String>): Int {
@@ -38,118 +109,27 @@ fun main() {
     }
 
     val testInput = readInput("Day${day}_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    check(part1(testInput) == 12521)
+//    check(part2(testInput) == 0)
 
     val input = readInput("Day${day}")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
 
-val costs = mapOf(
-    'A' to 1,
-    'B' to 10,
-    'C' to 100,
-    'D' to 1000
-)
-
-private fun Char.correctlyOccupied(pos: Int) = this@correctlyOccupied == "ABCD"[pos]
-private fun Char.isFree() = this@isFree == '.'
-
-data class Situation(val hallway: List<Char>, val rooms: List<List<Char>>, val cost: Int = 0)
-    :Comparable<Situation>{
-
-    override fun toString() = """
-        #############
-        #${hallway.joinToString("")}#
-        ###${rooms[0][0]}#${rooms[1][0]}#${rooms[2][0]}#${rooms[3][0]}###
-          #${rooms[0][1]}#${rooms[1][1]}#${rooms[2][1]}#${rooms[3][1]}#
-          #########""".trimMargin()
-
-    fun final() = rooms.foldIndexed(true) { i, a, b -> a && b.all { it.correctlyOccupied(i) } }
-
-    fun isPathFree(from: Int, to: Int) = hallway
-        .subList(minOf(from, to), maxOf(from, to) + 1)
-        .all { it.isFree() }
-
-    fun freeHallway() = hallway.indices.filter { i -> i !in listOf(2, 4, 6, 8) }
-
-    override fun compareTo(other: Situation) = this.cost.compareTo(other.cost)
-}
-
-private fun Situation.insertInHallway(pos: Int, char: Char): List<Char> {
-    val newHallway = mutableListOf<Char>().apply { addAll(this@insertInHallway.hallway) }
-    newHallway[pos] = char
-    return newHallway
-}
-
-private fun Situation.insertInRooms(room: Int, pos: Int, char: Char): List<List<Char>> {
-    val newRooms = mutableListOf<MutableList<Char>>().apply {
-        addAll(this@insertInRooms.rooms.map { it.toMutableList() }) }
-    newRooms[room][pos] = char
-    return newRooms
-}
-
-private fun transform(situation: Situation): List<Situation> {
-    val newSituations = mutableListOf<Situation>()
-
-    val frontRow = situation.rooms
-        .mapIndexed { i, room ->  i to room.indexOfFirst { it != '.' } }
-        .filter { it.second >= 0 }
-        .map { (it.first to it.second) to 1 + it.second }
-
-    for (front in frontRow) {
-        val (position, distance) = front
-        val (room, place) = position
-        val entrance = 2 + 2 * room
-        val amphipod = situation.rooms[room][place]
-        for (space in situation.freeHallway()) {
-            if (situation.isPathFree(space, entrance)) {
-                val newHallway = situation.insertInHallway(space, amphipod)
-                val newRooms = situation.insertInRooms(room, place,'.')
-                val cost = situation.cost + (distance + abs(space - entrance)) * costs[amphipod]!!
-                val newSituation = Situation(newHallway, newRooms, cost)
-                println("Transform")
-                println(newSituation)
-                println(cost)
-                val improvedSituations = moveToPlace(newSituation)
-                if (improvedSituations.isEmpty()) newSituations.add(newSituation)
-                else newSituations.addAll(improvedSituations)
-            }
-        }
+private data class Situation(val config: Array<CharArray>, val distance: Int) {
+    override fun equals(other: Any?): Boolean {
+        return other is Situation &&
+                (0..2).all { i -> config[i].contentEquals(other.config[i]) }
     }
-    return newSituations
-}
 
-private fun moveToPlace(situation: Situation): List<Situation> {
+    override fun hashCode() = (0..2).fold(0) { a, i -> a * 31 + config[i].contentHashCode() }
 
-    val newSituations = mutableListOf<Situation>()
+    fun copy(distance: Int) = Situation(config.map { it.copyOf() }.toTypedArray(), distance)
 
-    // Get all amphipods in hallway
-    for ((pos, amphipod) in situation.hallway.withIndex().filterNot { it.value.isFree() }) {
-        // Get home room for current amphipod and its entrance index
-        val home = "ABCD".indexOf(amphipod)
-        val entrance = 2 + 2*home
-        var homeSpace = -1
-        for ((homePos, content) in situation.rooms[home].reversed().withIndex()) {
-            if (content.isFree()) {
-                homeSpace = 1 - homePos
-                break
-            }
-            if (content != amphipod) break
-        }
-        val distance = 2 - homeSpace
-
-        // If the path to the entrance is free and there is a free space at home
-        if (situation.isPathFree(pos, entrance) && homeSpace != -1) {
-            val cost = situation.cost + (abs(pos - entrance) + distance) * costs[amphipod]!!
-            val newHallway = situation.insertInHallway(pos, '.')
-            val newRooms = situation.insertInRooms(home, homeSpace, amphipod)
-            val newSituation = Situation(newHallway, newRooms, cost)
-            println("Move to place")
-            println(newSituation)
-            newSituations.add(newSituation)
-        }
-    }
-    return newSituations
+    override fun toString() = buildList {
+        add("#".repeat(13))
+        addAll(config.map { it.concatToString() })
+        add("distance=$distance")
+    }.joinToString("\n")
 }
