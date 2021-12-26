639a95d1b694bf70ce5c51281e19ac38229bb05a
Marek Kohn <me@makohn.de>
2021-12-26 22:08:30 +0100

    Add solution for day 10 part 2

--- src/Day10.kt
+++ src/Day10.kt
@@ -2,7 +2,7 @@ fun main() {
 
     val day = "10"
 
-    val open = listOf('{','(','[','<')
+    val open = listOf('(','[','{','<')
 
     fun part1(input: List<String>): Int {
         var sum = 0
@@ -30,15 +30,33 @@ fun main() {
         return sum
     }
 
-    fun part2(input: List<String>): Int {
-        return -1
+    fun part2(input: List<String>): Long {
+        val scores = mutableListOf<Long>()
+        score@for (line in input) {
+            var sum = 0L
+            val stack = mutableListOf<Char>()
+            for (ch in line) {
+                val ret = when(ch) {
+                    in open -> stack.add(0, ch).let { ch }
+                    '}' -> if (stack.indexOfFirst{ it == '{' } == 0) stack.removeFirst() else ch
+                    ')' -> if (stack.indexOfFirst{ it == '(' } == 0) stack.removeFirst() else ch
+                    ']' -> if (stack.indexOfFirst{ it == '[' } == 0) stack.removeFirst() else ch
+                    '>' -> if (stack.indexOfFirst{ it == '<' } == 0) stack.removeFirst() else ch
+                    else -> error("Impossible")
+                }
+                if (ret !in open) continue@score
+            }
+            sum += stack.fold(0L) { acc, e -> 5 * acc + (open.indexOf(e) + 1) }
+            scores += sum
+        }
+        return scores.sorted()[scores.size/2]
     }
 
     val testInput = readInput("Day${day}_test")
     check(part1(testInput) == 26397)
-//    check(part2(testInput) == 0)
+    check(part2(testInput) == 288957L)
 
     val input = readInput("Day${day}")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
