750c9dd243ee1980c446c3b2290d216b8fce1bf8
Marek Kohn <me@makohn.de>
2022-01-01 16:02:20 +0100

    Add solution for day 19 part 2

--- src/Day19.kt
+++ src/Day19.kt
@@ -1,3 +1,4 @@
+import kotlin.math.abs
 
 fun main() {
 
@@ -5,20 +6,21 @@ fun main() {
 
     fun part1(input: List<String>): Int {
         val scanners = parse(input)
-        return countBeacons(scanners.map { it.toMutableSet() })
+        return solve(scanners.map { it.toMutableSet() }).first
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val scanners = parse(input)
+        return solve(scanners.map { it.toMutableSet() }).second
     }
 
     val testInput = readInput("Day${day}_test")
     check(part1(testInput) == 79)
-//    check(part2(testInput) == 0)
+    check(part2(testInput) == 3621)
 
     val input = readInput("Day${day}")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
 
 data class Vector3(val x: Int, val y: Int, val z: Int)
@@ -63,8 +65,8 @@ private fun findOverlaps(s1: Set<Vector3>, s2: Set<Vector3>): Pair<Int, Vector3>
     val rotations = s2.map { getRotations(it).toList() }
     var r = 11
     for (beacon in s1) {
-            for (rotation in rotations) {
-                for ((i, otherBeacon) in rotation.withIndex()) {
+        for (rotation in rotations) {
+            for ((i, otherBeacon) in rotation.withIndex()) {
                 val distance = beacon - otherBeacon
                 distances.getOrPut(distance) { mutableSetOf() }.add(otherBeacon)
                 if (distances[distance]!!.size >= 12) r = i
@@ -84,10 +86,12 @@ fun translate(vectors: Set<Vector3>, rotation: Int, offset: Vector3): MutableSet
     return rotated.map { it + offset }.toMutableSet()
 }
 
+fun manhattanDistance(v: Vector3, w: Vector3) = abs(v.x - w.x) + abs(v.y - w.y) + abs(v.z - w.z)
+
 typealias Translations = MutableMap<Pair<Int, Int>, Pair<Int, Vector3>>
 typealias Translation = Map.Entry<Pair<Int, Int>, Pair<Int, Vector3>>
 
-    private fun countBeacons(scanners: List<MutableSet<Vector3>>): Int {
+private fun solve(scanners: List<MutableSet<Vector3>>): Pair<Int, Int> {
     val translations = mutableMapOf<Pair<Int, Int>, Pair<Int, Vector3>>()
     for (i in 0 .. scanners.lastIndex) {
         for (j in 0 .. scanners.lastIndex) {
@@ -115,14 +119,25 @@ typealias Translation = Map.Entry<Pair<Int, Int>, Pair<Int, Vector3>>
     }
 
     val beacons = mutableSetOf<Vector3>()
+    val offsets = mutableMapOf( 0 to Vector3(0, 0, 0))
     beacons.addAll(scanners[0])
     for (i in 1..scanners.lastIndex) {
         val translation = findTranslations(0, i)
         var translated = scanners[i]
+        var offset = Vector3(0, 0, 0)
         for (t in translation) {
             translated = translate(translated, t.value.first, t.value.second)
+            offset = getRotations(offset).toList()[t.value.first] + t.value.second
         }
+        offsets[i] = offset
         beacons.addAll(translated)
     }
-    return beacons.size
+
+    val distances = mutableListOf<Int>()
+    for (i in 0 until offsets.size-1) {
+        for (j in i+1 until offsets.size) {
+            distances.add(manhattanDistance(offsets[i]!!, offsets[j]!!))
+        }
+    }
+    return beacons.size to distances.maxOf { it }
 }
