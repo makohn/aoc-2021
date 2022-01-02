0c49950396c90f39d71cc9406de2c85fe7e25f3e
Marek Kohn <me@makohn.de>
2022-01-02 18:56:24 +0100

    Add solution for day 17 part 2

--- src/Day17.kt
+++ src/Day17.kt
@@ -1,80 +1,67 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        Day17Helper.parse(input.first())
-        val (maxY, _) = Day17Helper.simulate()
+        val area = parse(input.first())
+        val (maxY, _) = simulate(area)
         return maxY
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val area = parse(input.first())
+        val (_, count) = simulate(area)
+        return count
     }
 
     val testInput = readInput("Day17_test")
     check(part1(testInput) == 45)
-//    check(part2(testInput) == 3)
+    check(part2(testInput) == 112)
 
     val input = readInput("Day17")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
 
-data class Vector(val x: Int, val y: Int, val vx: Int, val vy: Int)
+private fun parse(input: String): Quadruple<Int> {
+    val (minX, maxX, minY, maxY) = "(-?\\d+)".toRegex().findAll(input).map { it.value.toInt() }.toList()
+    return Quadruple(minX, maxX, minY, maxY)
+}
 
-object Day17Helper {
-
-    private var minX = 0
-    private var maxX = 0
-    private var minY = 0
-    private var maxY = 0
-
-    private const val maxTries = 1000
-
-    fun parse(input: String) {
-        val res = "(-?\\d+)".toRegex().findAll(input).map { it.value.toInt() }.toMutableList()
-        minX = res.removeFirst()
-        maxX = res.removeFirst()
-        minY = res.removeFirst()
-        maxY = res.removeFirst()
-    }
-
-    fun simulate(): Pair<Int, Int> {
-        var mY = 0
-        var count = 0
-        for (vy in -maxTries .. maxTries) {
-            var vx = 1
-            while (true) {
-                val res = shot(vx, vy)
-                if (res >= 0) count++
-                if (res > mY) mY = res
-                if (res == -3) break
-                if (vx >= maxTries) break
-                vx++
-            }
-        }
-        return mY to count
-    }
-
-    private fun shot(vx: Int, vy: Int): Int {
-        var vec = Vector(0, 0, vx, vy)
-        var mY = 0
-        while (true) {
-            vec = step(vec)
-            val (x, y, _, _) = vec
-            if (y > mY) mY = y
-            if ((x <= maxX) && (x >= minX) && (y <= maxY) && (y >= minY)) return mY
-            if ((y <= maxY) and (x > maxX)) return -3
-            if (y < minY)
-                return if (x < minX) -1
-                else -2
+private fun simulate(area: Quadruple<Int>): Pair<Int, Int> {
+    var yMax = 0
+    var count = 0
+    val (_, maxX, minY, maxY) = area
+    val yRange = maxOf(abs(minY), abs(maxY))
+    for (vx in 0 .. maxX) {
+        for (vy in -yRange .. yRange) {
+            val res = shot(vx, vy, area)
+            if (res >= 0) count++
+            if (res > yMax) yMax = res
         }
     }
+    return yMax to count
+}
 
-    private fun step(vec: Vector): Vector {
-        var (x, y, vx, vy) = vec
-        x += vx
-        y += vy
-        vx = if (vx > 0) vx - 1 else if (vx < 0) vx + 1 else vx
-        vy -= 1
-        return Vector(x, y, vx, vy)
+private fun shot(vx: Int, vy: Int, area: Quadruple<Int>): Int {
+    val (minX, maxX, minY, maxY) = area
+    var state = Quadruple(0, 0, vx, vy)
+    var yMax = 0
+    while (true) {
+        state = step(state)
+        val (x, y, svx, _) = state
+        if (y > yMax) yMax = y
+        if (x > maxX) return -1
+        if (svx == 0 && x !in (minX .. maxX)) return -1
+        if (svx == 0 && y < minY) return -1
+        if ((x in (minX .. maxX)) && y in (minY .. maxY)) return yMax
     }
+}
+
+private fun step(state: Quadruple<Int>): Quadruple<Int> {
+    var (x, y, vx, vy) = state
+    x += vx
+    y += vy
+    if (vx > 0) vx-- else if (vx < 0) vx++
+    vy--
+    return Quadruple(x, y, vx, vy)
 }
